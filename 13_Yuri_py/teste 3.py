d9019d63bc13a7c728e5ef9baedf4154d9561a1d
Arthur Mariano <thiesto@gmail.com>
2023-01-24 08:06:56 -0300

    Algoritmos de Flávio Yuri'

---
+++ 13_Yuri_py/teste 3.py
@@ -0,0 +1,6 @@
+# //Este é um código em phyton que lê o ano, mês e dia do usuário e print em tela os valores digitados
+print('Digite o dia mês e ano que você nasceu: ')
+dia = input ('digite o dia-> ')
+mes = input ('digite o mes-> ')
+ano = input ('digite o ano-> ')
+print ('Voce nasceu no dia',dia,'de',mes,'de',ano,'.','correto?')
---
+++ 13_Yuri_py/teste 4.py
@@ -0,0 +1,3 @@
+numero1= input ('Digite o primeiro número')
+numero2= input  ('Digite o segundo número')
+print ('A soma é:',numero1 + numero2)
