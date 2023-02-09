d80f98c0a4e6af9d7fff52839d07cc36335f62eb
Arthur Mariano NETO <thiesto@gmail.com>
2023-02-09 11:04:03 -0300

    JavaScript - menu parte três

--- 12_Infusion/JS/main.js
+++ 12_Infusion/JS/main.js
@@ -3,6 +3,13 @@
     $body.classList.remove('no-js');
     $body.classList.add('js');
 
-    var $btnMenu = document.queryCommandIndeterm.querySelector('.header__btnMenu');
-    $btnMenu.removeAttribute('style');
+    // var $btnMenu = document.queryCommandIndeterm.querySelector('.header__btnMenu');
+    // $btnMenu.removeAttribute('style');
+
+    var menu = new Menu({
+        conteiner: '.header__nav',
+        toggleBtn: '.header__btnMenu',
+        witdhEnabled: 1024
+
+    })
 })()
--- 12_Infusion/JS/menu.js
+++ 12_Infusion/JS/menu.js
@@ -1,5 +1,33 @@
-(function(){
-    var $body = document.querySelector('body');
-    $body.classList.remove('no-js');
-    $body.classList.add('js');
-})()
+function Menu(config) {
+    this.nav = (typeof config.cainter === 'string') ?
+        document.querySelector(config.container) : config.container;
+
+    this.btn = (typeof config.toggleBtn === 'string') ?
+        document.querySelector(config.toggleBtn) : config.toggleBtn;
+
+    this.maxWidth = config.widthEnable || false;
+    
+    var _opened = false;
+    var _this = this;
+
+    this.btn.removeAttribute('style');
+
+    this.btn.addEventListener('click', openOrClose)
+
+    function openOrClose(){
+        if(!_opened){
+            openMenu()
+        } else {
+            closeMenu()
+        }
+    }
+
+    function openMenu(){
+        _opened = true;
+
+    }
+
+    function closeMenu(){
+        _opened = false;
+    }
+}
